b6714df17ae36686391c855bb5bb7db6ace42549
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2021-03-26 15:55:03 +0100

    update build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -94,6 +94,7 @@ fun makeTest(
         file: String,
         name: String = file,
         sampling: Double = 1.0,
+        effect : String = "",
         time: Double = Double.POSITIVE_INFINITY,
         vars: Set<String> = setOf(),
         maxHeap: Long? = null,
@@ -144,6 +145,10 @@ fun makeTest(
 
         if (vars.isNotEmpty()) {
             args("-b", "-var", *vars.toTypedArray(), "--headless")
+        }
+
+        if(effect != "") {
+            args("-g", "./src/main/resources/${effect}.aes")
         }
     }
     /*tasks {
